e3128e35a4e288af2d5bc6267edd19532120adb8
Mikko Ohtamaa <mikko@redinnovation.com>
2017-08-22 17:55:34 +0300

    Mikko's experimental version

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ docs_extras = [
 ]
 
 setup(name='pyramid_tm',
-      version='0.12.1',
+      version='0.12.1+experimental',
       description=('A package which allows Pyramid requests to join the '
                    'active transaction'),
       long_description=README + '\n\n' + CHANGES,
